437b2fbb45c377c1389b28c882d082c00f2b9fdd
Andrey G <a.gavrilov@promo-bot.ru>
2022-10-14 14:30:42 +0500

    UI button component

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button text="Add Task" color="green" />
+    <Button text="Update Task" color="blue" />
+    <Button text="Delete Task" color="red" />
+  </header>
+</template>
+
+<script>
+import Button from "./Button.vue";
+
+export default {
+  name: "Header",
+  props: {
+    title: String,
+    // title: {
+    //   type: String,
+    //   default: "Hello default",
+    // },
+  },
+  components: {
+    Button,
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
